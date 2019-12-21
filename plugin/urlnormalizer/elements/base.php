ac6d37016f9f717ed7cf70f02ab26a53dde005e8
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-12-21 18:30:43 +0200

    Update elements

--- plugin/urlnormalizer/elements/base.php
+++ plugin/urlnormalizer/elements/base.php
@@ -10,7 +10,30 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-if (version_compare(JVERSION, '2.5.0', 'ge')) {
+if (version_compare(JVERSION, '3.5.0', 'ge')) {
+    jimport('joomla.form.formfield');
+    class JWElement extends JFormField
+    {
+        public function getInput()
+        {
+            return $this->fetchElement($this->name, $this->value, $this->element, $this->options['control']);
+        }
+
+        public function getLabel()
+        {
+            if (method_exists($this, 'fetchTooltip')) {
+                return $this->fetchTooltip($this->element['label'], $this->description, $this->element, $this->options['control'], $this->element['name'] = '');
+            } else {
+                return parent::getLabel();
+            }
+        }
+
+        public function render($layoutId, $data = array())
+        {
+            return $this->getInput();
+        }
+    }
+} elseif (version_compare(JVERSION, '2.5.0', 'ge')) {
     jimport('joomla.form.formfield');
     class JWElement extends JFormField
     {
--- plugin/urlnormalizer/elements/header.php
+++ plugin/urlnormalizer/elements/header.php
@@ -10,21 +10,75 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-require_once(dirname(__FILE__).'/base.php');
+require_once dirname(__FILE__).'/base.php';
 
 class JWElementHeader extends JWElement
 {
     public function fetchElement($name, $value, &$node, $control_name)
     {
-        $document = JFactory::getDocument();
-
-        if (version_compare(JVERSION, '1.6.0', 'ge')) {
-            $document->addStyleSheet(JURI::root(true).'/plugins/system/urlnormalizer/urlnormalizer/elements/header.css?v=1.5');
-            return '<div class="jwHeaderContainer"><div class="jwHeaderContent">'.JText::_($value).'</div><div class="jwHeaderClr"></div></div>';
+        $plg_name = "urlnormalizer";
+        if (version_compare(JVERSION, '2.5.0', 'ge')) {
+            $pluginLivePath = JURI::root(true).'/plugins/content/'.$plg_name.'/'.$plg_name;
         } else {
-            $document->addStyleSheet(JURI::root(true).'/plugins/system/urlnormalizer/elements/header.css?v=1.5');
-            return '<div class="jwHeaderContainer15"><div class="jwHeaderContent">'.JText::_($value).'</div><div class="jwHeaderClr"></div></div>';
+            $pluginLivePath = JURI::root(true).'/plugins/content/'.$plg_name;
         }
+        $document = JFactory::getDocument();
+        $document->addStyleSheet($pluginLivePath.'/includes/elements/header.css?v=1.5');
+
+        $cssClass = '';
+        if (version_compare(JVERSION, '1.6.0', 'lt')) {
+            $cssClass = '15';
+        }
+        return '<div class="jwHeaderContainer'.$cssClass.'"><div class="jwHeaderContent">'.JText::_($value).'</div><div class="jwHeaderClr"></div></div>';
+    }
+
+    public function fetchTooltip($label, $description, &$node, $control_name, $name)
+    {
+        return null;
+    }
+}
+
+class JFormFieldHeader extends JWElementHeader
+{
+    public $type = 'header';
+}
+
+class JElementHeader extends JWElementHeader
+{
+    public $_name = 'header';
+}
+<?php
+/**
+ * @version    5.2.0
+ * @package    AllVideos (plugin)
+ * @author     JoomlaWorks - http://www.joomlaworks.net
+ * @copyright  Copyright (c) 2006 - 2019 JoomlaWorks Ltd. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once dirname(__FILE__).'/base.php';
+
+class JWElementHeader extends JWElement
+{
+    public function fetchElement($name, $value, &$node, $control_name)
+    {
+        $plg_name = "jw_allvideos";
+        if (version_compare(JVERSION, '2.5.0', 'ge')) {
+            $pluginLivePath = JURI::root(true).'/plugins/content/'.$plg_name.'/'.$plg_name;
+        } else {
+            $pluginLivePath = JURI::root(true).'/plugins/content/'.$plg_name;
+        }
+        $document = JFactory::getDocument();
+        $document->addStyleSheet($pluginLivePath.'/includes/elements/header.css');
+
+        $cssClass = '';
+        if (version_compare(JVERSION, '1.6.0', 'lt')) {
+            $cssClass = '15';
+        }
+        return '<div class="jwHeaderContainer'.$cssClass.'"><div class="jwHeaderContent">'.JText::_($value).'</div><div class="jwHeaderClr"></div></div>';
     }
 
     public function fetchTooltip($label, $description, &$node, $control_name, $name)
