a681c3b1dd28b466153c02c1077904062efc3828
Fotis Evangelou <fevangelou@users.noreply.github.com>
2021-04-22 23:08:22 +0300

    Update dates

--- plugin/urlnormalizer/elements/base.php
+++ plugin/urlnormalizer/elements/base.php
@@ -3,7 +3,7 @@
  * @version    1.8
  * @package    URL Normalizer (plugin)
  * @author     JoomlaWorks - https://www.joomlaworks.net
- * @copyright  Copyright (c) 2006 - 2020 JoomlaWorks Ltd. All rights reserved.
+ * @copyright  Copyright (c) 2006 - 2021 JoomlaWorks Ltd. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/licenses/gpl.html
  */
 
